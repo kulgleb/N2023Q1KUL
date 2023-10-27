15e2a5cc3a627063e088b7a57ed1306e3aab6de3
_ _ <_@example.com>
2023-10-27 11:22:38 +0300

    add: weekdaysearch.js

---
+++ variable.js
@@ -0,0 +1,7 @@
+const nnumber = 7
+const bnumber = 8
+const result = nnumber +bnumber
+const rword = 'Привет'
+const pword = 'мир'
+const rresult = rword + pword 
+console.log(result)
---
+++ weekdaysearch.js
@@ -0,0 +1,17 @@
+const numericRepresentationOfTheDayOfTheWeek = 5
+
+if (numericRepresentationOfTheDayOfTheWeek === 1)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - Это Понедельник`)
+else if (numericRepresentationOfTheDayOfTheWeek === 2)
+    console.log(`${numericRepresentationOfTheDayOfTheWeek} - Это Вторник`)
+else if (numericRepresentationOfTheDayOfTheWeek === 3)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Среда`)
+else if (numericRepresentationOfTheDayOfTheWeek === 4)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Четверг`)
+else if (numericRepresentationOfTheDayOfTheWeek === 5)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Пятница`)
+else if (numericRepresentationOfTheDayOfTheWeek === 6)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Суббота`)
+else if (numericRepresentationOfTheDayOfTheWeek === 7)
+    console.log(`${ numericRepresentationOfTheDayOfTheWeek } - Это Воскресенье`)
+else console.log('Неверные данные')
